38dcc57e430c2b2196f51ce25b83a355713655c2
AMANSHU <amanshu@khoslalabs.com>
2019-04-22 00:03:34 +0530

    empty face animation

--- app/src/main/java/io/github/amanshuraikwar/howmuch/ui/list/empty/EmptyViewHolder.kt
+++ app/src/main/java/io/github/amanshuraikwar/howmuch/ui/list/empty/EmptyViewHolder.kt
@@ -1,11 +1,13 @@
 package io.github.amanshuraikwar.howmuch.ui.list.empty
 
+import android.graphics.drawable.Drawable
 import androidx.annotation.LayoutRes
 import androidx.fragment.app.FragmentActivity
 import android.view.View
+import androidx.vectordrawable.graphics.drawable.Animatable2Compat
+import androidx.vectordrawable.graphics.drawable.AnimatedVectorDrawableCompat
 import io.github.amanshuraikwar.multiitemlistadapter.ViewHolder
 import io.github.amanshuraikwar.howmuch.R
-import io.github.amanshuraikwar.howmuch.base.util.Util;
 import kotlinx.android.synthetic.main.item_empty.view.*
 
 class EmptyViewHolder(itemView: View) : ViewHolder<EmptyListItem>(itemView) {
@@ -15,7 +17,21 @@ class EmptyViewHolder(itemView: View) : ViewHolder<EmptyListItem>(itemView) {
         val LAYOUT = R.layout.item_empty
     }
 
-    override fun bind(listItem: EmptyListItem, host: androidx.fragment.app.FragmentActivity) {
-        itemView.messageTv.text = listItem.message
+    override fun bind(listItem: EmptyListItem, host: FragmentActivity) {
+
+        val animated =
+                AnimatedVectorDrawableCompat.create(host, R.drawable.avd_face_looking)
+
+        animated?.registerAnimationCallback(
+                object : Animatable2Compat.AnimationCallback() {
+                    override fun onAnimationEnd(drawable: Drawable?) {
+                        itemView.iv.post { animated.start() }
+                    }
+                }
+        )
+
+        itemView.iv.setImageDrawable(animated)
+        animated?.start()
+
     }
 }
