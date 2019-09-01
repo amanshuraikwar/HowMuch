aebe09de111196a0e0713554720e713d1f623eca
Amanshu Raikwar <amanshuraikwar.in@gmail.com>
2019-09-01 16:06:10 +0530

    removed playground module

--- playground/src/main/java/io/github/amanshuraikwar/playground/data/DataManager.kt
+++
@@ -1,9 +0,0 @@
-package io.github.amanshuraikwar.playground.data
-
-import io.github.amanshuraikwar.howmuch.protocol.UserDataManager
-import io.reactivex.Observable
-
-interface DataManager : UserDataManager {
-
-    fun getAllSongs(): Observable<List<Song>>
-}
